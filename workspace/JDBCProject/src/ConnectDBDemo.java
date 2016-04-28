a91f95b3d9a217db34e3cfc03fdf537f577eba3a
puneetvashisht <puneetvashisht@gmail.com>
2016-04-28 16:28:50 +0530

    change

--- workspace/JDBCProject/src/ConnectDBDemo.java
+++ workspace/JDBCProject/src/ConnectDBDemo.java
@@ -8,10 +8,15 @@ public class ConnectDBDemo {
 
 	public static void main(String[] args) {
 		try {
+			/*For Oracle Use
+			Class.forName("oracle.jdbc.driver.OracleDriver"); */
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("Load the Driver...");
 			try {
 				Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/sapientdemodb","root","root");
+				/*For Oracle USe
+				"jdbc:oracle:thin:@localhost:1521:xe","system","oracle"*/
+				
 				System.out.println("Connection Created...");
 				
 				Statement stmt=con.createStatement();  
