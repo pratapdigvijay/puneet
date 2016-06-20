9d90c5be5a4897f1d0c06191f4a4b0aec42168bd
puneetvashisht <puneetvashisht@gmail.com>
2016-06-20 11:05:15 +0530

    JPA

---
+++ workspace/JPADemo/src/com/sapient/entity/TestEmployee.java
@@ -0,0 +1,41 @@
+package com.sapient.entity;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+
+		Employee employee = new Employee("Priya");
+
+		// HIbernate Code
+
+		// Step 1. Create a SessionFactory and in JPA we call it
+		// EntityManagerFactory
+
+		// SessionFactory sessionFactory = new
+		// Configuration().configure().buildSessionFactory();
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("myPersistanceUnit");
+
+		// Step 2. Get a session from sessionFactory and we call it
+		// EntityManager
+
+		// Session session = sessionFactory.openSession();
+		EntityManager em = emf.createEntityManager();
+
+		// Transaction transaction = session.beginTransaction();
+		EntityTransaction transaction = em.getTransaction();
+
+		transaction.begin();
+
+		em.persist(employee);
+
+		transaction.commit();
+		em.close();
+
+	}
+}
